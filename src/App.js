0458fc53d44557d2d77c760fe7cd81c8e3119940
Mariia Lishchynskaya <randymasha33@gmail.com>
2019-09-09 17:15:35 +0300

    "created file for constants, added in it menu and import this constant to App and Header"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import './App.css';
+import './constants/text';
 import Header from './components/Header/Header'
 
 
 class App extends React.Component {
 
   render() {
-    const menu = {
-      title: 'My header',
-      menuItems: ['Home', 'Posts', 'Contact']
-    };
-    return (Header(menu));
+    return (<Header/>);
   }
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import menu from '../../constants/text';
 import './Header.css';
 
 
-const Header = ({title, menuItems}) => (
-    <header className="header">
+const Header = () => {
+    let {title, menuItems} = menu;
+    return (<header className="header">
         <h1 className="headline">{title}</h1>
         {
             menuItems.map(item => (<li className="header-item" key={item}>{item}</li>))
         }
-    </header>
-);
+    </header>)
+};
 
 export default Header;
