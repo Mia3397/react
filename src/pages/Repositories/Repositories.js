7dc0c337cc70e93baefd0c6bc572af6c275bac6c
Mariia Lishchynskaya <randymasha33@gmail.com>
2019-09-12 21:20:15 +0300

    created input component

--- src/pages/Repositories/Repositories.js
+++ src/pages/Repositories/Repositories.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Input from '../../components/Input/Input';
 import './Repositories.css';
 
-const Repositories = () => (
-    <div>
-        <h1>Repositories</h1>
-    </div>
-);
+class Repositories extends Component {
+    state = {
+        email: '',
+        name: ''
+    };
+
+    onChange = ({target}) => {
+        const {name, value} = target;
+        this.setState({
+            [name]: value
+        })
+    };
+
+
+    render() {
+        const {email, name} = this.state;
+        return (
+            <div className="wrapper">
+                <h1>Repositories</h1>
+                <div className="input-wrapper">
+                    <Input
+                        name="email"
+                        onChange={this.onChange}
+                        placeholder="Email"
+                        value={email}
+
+                    />
+                    <Input
+                        name="name"
+                        onChange={this.onChange}
+                        placeholder="Name"
+                        value={name}
+                    />
+                </div>
+
+            </div>
+        )
+    }
+}
 
 export default Repositories;
