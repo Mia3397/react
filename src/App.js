7c4c44cf265dc0064623e2c3fc5bffec63ff3a90
Mariia Lishchynskaya <randymasha33@gmail.com>
2019-09-12 14:26:41 +0300

    deleted switch

--- src/App.js
+++ src/App.js
@@ -3,20 +3,18 @@ import './App.css';
 import './constants/text';
 import {Header, Sidebar} from './components';
 import {Repositories, Chart, Notes, Price} from "./pages";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App = () => (
     <Router>
         <Header/>
         <main>
             <Sidebar/>
-            <Switch>
-                <Route path="/" exact component={Repositories}/>
-                <Route path="/repositories" component={Repositories}/>
-                <Route path="/chart" component={Chart}/>
-                <Route path="/price" component={Price}/>
-                <Route path="/notes" component={Notes}/>
-            </Switch>
+            <Route path="/" exact component={Repositories}/>
+            <Route path="/repositories" component={Repositories}/>
+            <Route path="/chart" component={Chart}/>
+            <Route path="/price" component={Price}/>
+            <Route path="/notes" component={Notes}/>
         </main>
     </Router>
 );
