99a3772b93a2ffc3cefd9a90aed4c8af8c7e5e83
Denis <denis167518@gmail.com>
2019-10-06 21:45:02 +0300

    Updates

---
+++ precache-manifest.ffda1e905bbf8170f86809cbaaaa95f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ada2b9342ea2ef6a843356d1d5f75cde",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6f3f0f865a8c21e7cc49",
+    "url": "/react/static/css/2.96121624.chunk.css"
+  },
+  {
+    "revision": "ba3973ccf2131cd924d3",
+    "url": "/react/static/css/main.46c3b9bb.chunk.css"
+  },
+  {
+    "revision": "6f3f0f865a8c21e7cc49",
+    "url": "/react/static/js/2.c53ef6b6.chunk.js"
+  },
+  {
+    "revision": "ba3973ccf2131cd924d3",
+    "url": "/react/static/js/main.551572b1.chunk.js"
+  },
+  {
+    "revision": "c84c2fd0455e4c33fdea",
+    "url": "/react/static/js/runtime~main.cc7944de.js"
+  }
+]);
